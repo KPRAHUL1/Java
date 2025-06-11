238c014b954c16c700fb9b56338a2dfe3452ed99
KPRAHUL1 <rahulsachin07314@gmail.com>
2025-06-11 22:27:38 +0530

    patterns

---
+++ pattern.java
@@ -0,0 +1,38 @@
+public class pattern {
+    public  static  void main(String[] args) {
+//        for (int i=1;i<=5;i++){
+//            for (int j=1;j<=i;j++){
+//                System.out.print('*');
+//            }
+//            System.out.print("\n");
+//        }
+//        for (int i = 1; i <= 5; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print(i);
+//            }
+//            System.out.print("\n");
+//        }
+//        for (int i = 1; i <= 5; i++) {
+//           for (int j = 1; j <= i; j++) {
+//               System.out.print(j);
+//           }
+//           System.out.print("\n");
+//       }
+//        int num=1;
+//        for (int i = 0; i <= 5; i++) {
+//            for (int j = 1; j <= i; j++) {
+//              System.out.print(num);
+//              num++;
+//            }
+//            System.out.print("\n");
+//        }
+//        for (int i = 5; i >= 1; i--) {
+//            for (int j = 1; j <= i; j++) {
+//               System.out.print(j);
+//           }
+//            System.out.print("\n");
+//        }
+
+
+    }
+}
