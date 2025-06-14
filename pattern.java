3566a0de7290f2dbd3b2dbab50664c4b00397fdb
KPRAHUL1 <rahulsachin07314@gmail.com>
2025-06-14 22:28:54 +0530

    patterns prime pattern

--- pattern.java
+++ pattern.java
@@ -33,6 +33,15 @@ public class pattern {
 //            System.out.print("\n");
 //        }
 
+        for(int i=1;i<=5;i++){
+            for (int j=1;j<=5;j++){
+                if ( (i==2 && j==2) || (i==3 && j==4) || (i==4 && j==2)){
+                    break;
+                }
+                System.out.print('*');
+            }
+            System.out.print("\n");
+        }
 
     }
 }
