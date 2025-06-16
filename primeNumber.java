4ed5271d2ad225fe4d304e0a9ed5b81747deba83
KPRAHUL1 <rahulsachin07314@gmail.com>
2025-06-16 21:41:51 +0530

    patterns prime pattern

--- primeNumber.java
+++ primeNumber.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class primeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Number:");
+        int num = scanner.nextInt();
+        if (num % 1==0){
+            System.out.println("It's a Prime Number");
+        }
+        else {
+            System.out.println("It's Not a Prime Number");
+        }
+    }
 }
