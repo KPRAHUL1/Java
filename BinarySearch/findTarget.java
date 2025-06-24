d853863be8f132498a545f6f192e6b7bfe4fdbb4
KPRAHUL1 <rahulsachin07314@gmail.com>
2025-06-24 22:27:45 +0530

    Binary Search

---
+++ BinarySearch/findTarget.java
@@ -0,0 +1,24 @@
+package BinarySearch;
+
+public class findTarget {
+    public static void main(String[] args) {
+        int[] arr ={2,3,5,9,14,16,18};
+        int target = 18;
+        int start = 0;
+        int end = arr.length;
+
+        while(start<=end){
+            int mid = start +(end-start)/2;
+           if (target< arr[mid]) {
+               end = mid-1;
+           } else if (target>arr[mid]) {
+               start=mid+1;
+           }
+           else{
+               System.out.println(arr[mid]);
+               return;
+           }
+        }
+        System.out.println(arr[start]);
+    }
+}
